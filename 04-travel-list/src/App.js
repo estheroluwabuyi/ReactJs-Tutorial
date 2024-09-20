d4dea8e2fd5d9db752c129bfd24d00c24666f014
Esther Okiki <estherokiki15@gmail.com>
2024-09-20 16:35:15 +0100

    Building Layout

--- 04-travel-list/src/App.js
+++ 04-travel-list/src/App.js
@@ -1,6 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Laptop", quantity: 1, packed: true },
 ];
 
 export default function App() {
@@ -27,9 +28,26 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((i) => (
+          <Item item={i} />
+        ))}
+      </ul>
+    </div>
+  );
 }
-
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
 function Stats() {
   return (
     <footer className="stats">
