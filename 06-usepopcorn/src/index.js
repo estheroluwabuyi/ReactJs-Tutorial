393f98403a16e9f8a5af038b1c8db6f836a606b2
Esther Okiki <estherokiki15@gmail.com>
2024-11-14 10:21:10 +0100

    how to NOT fetch data that prooduces side effect and how to fetch data using the useEffect hook

--- 06-usepopcorn/src/index.js
+++ 06-usepopcorn/src/index.js
@@ -1,35 +1,36 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
+import "./index.css";
 // import App from "./App";
-import StarRating from "./StarRating";
+import App from "./App-v1";
+// import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating
-        color="blue"
-        maxRating={10}
-        onSetRatingHandler={setMovieRating}
-      />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating
+//         color="blue"
+//         maxRating={10}
+//         onSetRatingHandler={setMovieRating}
+//       />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
-    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <StarRating size={24} color="red" className="test" defaultRating={3} /> */}
     {/* <StarRating maxRating={10} />
     <StarRating /> */}
 
-    <Test />
+    {/* <Test /> */}
   </React.StrictMode>
 );
