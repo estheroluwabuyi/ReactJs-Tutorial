02d263e208fabbb3797e98fb6ebe55bd68f8a761
Esther Okiki <estherokiki15@gmail.com>
2024-12-17 22:14:40 +0100

    child to parent communication

--- 08-classy-weather/src/App.js
+++ 08-classy-weather/src/App.js
@@ -75,18 +75,17 @@ class App extends React.Component {
     }
   };
 
+  setLocation = (e) => this.setState({ location: e.target.value });
+
   render() {
     return (
       <div className="app">
         <h1>Classy Weather</h1>
-        <div>
-          <input
-            type="text"
-            placeholder="Search for location..."
-            value={this.state.location}
-            onChange={(e) => this.setState({ location: e.target.value })}
-          />
-        </div>
+        <Input
+          location={this.state.location}
+          onChangeLocation={this.setLocation}
+        />
+
         <button onClick={this.fetchWeather}>Get weather</button>
 
         {this.state.isLoading && <p className="loader">Loading...</p>}
@@ -103,6 +102,19 @@ class App extends React.Component {
 }
 
 export default App;
+
+class Input extends React.Component {
+  render() {
+    return (
+      <input
+        type="text"
+        placeholder="Search for location..."
+        value={this.props.location}
+        onChange={this.props.onChangeLocation}
+      />
+    );
+  }
+}
 
 class Weather extends React.Component {
   render() {
