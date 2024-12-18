a5bcedc641227e0c18a5efc10c3c8bdf1e84ecc3
Esther Okiki <estherokiki15@gmail.com>
2024-12-18 21:21:29 +0100

    lifecycle methods (class-based function)

--- 08-classy-weather/src/App.js
+++ 08-classy-weather/src/App.js
@@ -35,7 +35,7 @@ function formatDay(dateStr) {
 ///////////////////////////////////////////////////////////////////
 class App extends React.Component {
   state = {
-    location: "Nigeria",
+    location: "",
     isLoading: false,
     displayLocation: "",
     weather: {},
@@ -43,6 +43,8 @@ class App extends React.Component {
 
   // async fetchWeather(){
   fetchWeather = async () => {
+    if (this.state.location.length < 2) return this.setState({ weather: {} });
+
     try {
       this.setState({ isLoading: true });
       // 1) Getting location (geocoding)
@@ -50,7 +52,7 @@ class App extends React.Component {
         `https://geocoding-api.open-meteo.com/v1/search?name=${this.state.location}`
       );
       const geoData = await geoRes.json();
-      console.log(geoData);
+      // console.log(geoData);
 
       if (!geoData.results) throw new Error("Location not found");
 
@@ -65,17 +67,37 @@ class App extends React.Component {
         `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
       );
       const weatherData = await weatherRes.json();
-      console.log(weatherData);
+      // console.log(weatherData);
 
       this.setState({ weather: weatherData.daily });
     } catch (err) {
-      console.err(err);
+      console.error(err);
     } finally {
       this.setState({ isLoading: false });
     }
   };
 
   setLocation = (e) => this.setState({ location: e.target.value });
+
+  // Lifecycle Methods
+  //useEffect []
+  //Called immediately after rendering
+  componentDidMount() {
+    // this.fetchWeather();
+    //this will immediately start fetching for the weather as soon as the app is loaded. This works because we have Nigeria ass a predefined location string
+
+    this.setState({ location: localStorage.getItem("location") || "" });
+  }
+
+  //useEffect [location]
+  //react gives it access to previous state and previous props. Called after rerenders
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+
+      localStorage.setItem("location", this.state.location);
+    }
+  }
 
   render() {
     return (
@@ -85,11 +107,8 @@ class App extends React.Component {
           location={this.state.location}
           onChangeLocation={this.setLocation}
         />
-
-        <button onClick={this.fetchWeather}>Get weather</button>
-
+        {/* <button onClick={this.fetchWeather}>Get weather</button> */}
         {this.state.isLoading && <p className="loader">Loading...</p>}
-
         {this.state.weather.weathercode && (
           <Weather
             weather={this.state.weather}
@@ -104,6 +123,11 @@ class App extends React.Component {
 export default App;
 
 class Input extends React.Component {
+  //runs after the component unmounts
+  componentWillUnmount() {
+    console.log("Weather will unmount");
+  }
+
   render() {
     return (
       <input
