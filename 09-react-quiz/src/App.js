d59ffb1c681bc8a1af9ec58195c61153251e88a9
Esther Okiki <estherokiki15@gmail.com>
2024-12-26 21:56:29 +0100

    setting up the react quiz app

--- 09-react-quiz/src/App.js
+++ 09-react-quiz/src/App.js
@@ -1,7 +1,17 @@
-import DateCounter from "./DateCounter"
+import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
-    return <div>
-        <DateCounter/>
-         </div>
+  return (
+    <div className="app">
+      <Header />
+
+      <main className="main">
+        <Main>
+          <p>1/15</p>
+          <p>Questions?</p>
+        </Main>
+      </main>
+    </div>
+  );
 }
