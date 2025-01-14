2c66be7acb210ea8c6492f1b1f338967de8e0b7b
Esther Okiki <estherokiki15@gmail.com>
2025-01-14 17:23:45 +0100

    implementing main pages and routes

---
+++ 10-worldwise/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
+
+function App() {
+  return (
+    <div>
+      {/* <h1>Hello Router!</h1> 
+      this page will show irrespective of the currently displayed component
+      */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+
+          <Route path="*" element={<PageNotFound />} />
+          {/* this will catch all the paths that were not matched to one of the 3 */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ 10-worldwise/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+// import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+//unlike in create-react-app, vite uses main.jsx instead of index.jsx
