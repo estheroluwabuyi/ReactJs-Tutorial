6dcb67a61084951e1a23bf44f6f27197fbfc9dc1
Esther Okiki <estherokiki15@gmail.com>
2025-01-18 22:12:50 +0100

    nested routes

--- 10-worldwise/src/App.jsx
+++ 10-worldwise/src/App.jsx
@@ -19,7 +19,12 @@ function App() {
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="login" element={<Login />} />
-          <Route path="app" element={<AppLayout />} />
+
+          <Route path="app" element={<AppLayout />}>
+            <Route path="cities" element={<p>List of cities</p>} />
+            <Route path="countries" element={<p>Countries</p>} />
+            <Route path="form" element={<p>Form</p>} />
+          </Route>
 
           <Route path="*" element={<PageNotFound />} />
           {/* this will catch all the paths that were not matched to one of the 3 */}
