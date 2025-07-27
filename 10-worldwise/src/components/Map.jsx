60d390009020d9c24138b3ec36b474d6845565ff
Esther Okiki <estherokiki15@gmail.com>
2025-07-27 19:59:31 +0100

    Reading and Setting a Query String

--- 10-worldwise/src/components/Map.jsx
+++ 10-worldwise/src/components/Map.jsx
@@ -1,7 +1,29 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  //k=just like useState, but for query parameters
+  //v=useSearchParams is a hook that returns an array with two elements:
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h1>
+        Position: {lat},{lng}
+      </h1>
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 23, lng: 50 });
+        }}
+      >
+        Change POS
+      </button>
+    </div>
+  );
 }
 
 export default Map;
