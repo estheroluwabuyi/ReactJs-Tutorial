4b0765632ff5f61e6655ad2e6458e9b9874bab1f
Esther Okiki <estherokiki15@gmail.com>
2025-07-29 10:03:41 +0100

    Programatic navigation with useNavigate

--- 10-worldwise/src/components/Map.jsx
+++ 10-worldwise/src/components/Map.jsx
@@ -1,7 +1,9 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
+  const navigate = useNavigate();
+
   //k=just like useState, but for query parameters
   //v=useSearchParams is a hook that returns an array with two elements:
   const [searchParams, setSearchParams] = useSearchParams();
@@ -10,7 +12,12 @@ function Map() {
   const lng = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer}>
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form");
+      }}
+    >
       <h1>Map</h1>
       <h1>
         Position: {lat},{lng}
