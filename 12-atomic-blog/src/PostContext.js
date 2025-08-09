ae492ff647d1c8d67460bb454cb67ec67bf17669
Esther Okiki <estherokiki15@gmail.com>
2025-08-09 06:45:45 +0100

    Advanced Pattern: Custom Provider and Hook

---
+++ 12-atomic-blog/src/PostContext.js
@@ -0,0 +1,62 @@
+import { createContext, useContext, useState } from "react";
+import { faker } from "@faker-js/faker";
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+
+// 1) CREATE A NEW CONTEXT
+const PostContext = createContext();
+
+function PostProvider({ children }) {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  return (
+    // 2) PROVIDE TO THE CHILD COMPONENT
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onAddPost: handleAddPost,
+        onClearPosts: handleClearPosts,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  );
+}
+
+// Custom Hook
+function usePosts() {
+  const context = useContext(PostContext);
+
+  if (context === undefined)
+    throw new Error("Post Context was used outside of the PostProvider");
+  return context;
+}
+
+export { PostProvider, usePosts };
