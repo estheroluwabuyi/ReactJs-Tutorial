dd33156c016ceb18e5574621801d0e2e260b05f7
Esther Okiki <estherokiki15@gmail.com>
2025-08-12 09:08:17 +0100

    Creating CitiesContxt

--- 10-worldwise/src/App.jsx
+++ 10-worldwise/src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Product from "./pages/Product";
@@ -11,72 +10,44 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-
-const BASE_URL = "http://localhost:5000";
+import { CitiesProvider } from "./contexts/CitiesContext";
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(function () {
-    async function fetchCities() {
-      // setIsLoading(true);
-      try {
-        setIsLoading(true);
-
-        const response = await fetch(`${BASE_URL}/cities`);
-        const data = await response.json();
-        setCities(data);
-        // console.log(data);
-      } catch {
-        console.error("There was an error fetching data...");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
-
   return (
     <div>
       {/* <h1>Hello Router!</h1> 
       this page will show irrespective of the currently displayed component
       */}
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            {/* <Route path="/" element={<HomePage />} /> */}
+            <Route index element={<HomePage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="login" element={<Login />} />
 
-      <BrowserRouter>
-        <Routes>
-          {/* <Route path="/" element={<HomePage />} /> */}
-          <Route index element={<HomePage />} />
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="login" element={<Login />} />
-
-          <Route path="app" element={<AppLayout />}>
-            {/* <Route
+            <Route path="app" element={<AppLayout />}>
+              {/* <Route
               index
               element={<CityList cities={cities} isLoading={isLoading} />}
             /> */}
-            <Route index element={<Navigate to="cities" replace />} />
-            {/* This will be the default route in App Layout */}
-            <Route
-              path="cities"
-              element={<CityList cities={cities} isLoading={isLoading} />}
-            />
+              <Route index element={<Navigate to="cities" replace />} />
+              {/* This will be the default route in App Layout */}
+              <Route path="cities" element={<CityList />} />
 
-            {/* dynamic rotes with url params */}
-            <Route path="cities/:id" element={<City />} />
+              {/* dynamic rotes with url params */}
+              <Route path="cities/:id" element={<City />} />
 
-            <Route
-              path="countries"
-              element={<CountryList cities={cities} isLoading={isLoading} />}
-            />
-            <Route path="form" element={<Form />} />
-          </Route>
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
 
-          <Route path="*" element={<PageNotFound />} />
-          {/* this will catch all the paths that were not matched to one of the 3 */}
-        </Routes>
-      </BrowserRouter>
+            <Route path="*" element={<PageNotFound />} />
+            {/* this will catch all the paths that were not matched to one of the 3 */}
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
     </div>
   );
 }
