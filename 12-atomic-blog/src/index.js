536430fb0666bdad4e9f19d4c440d575692adaed
Esther Okiki <estherokiki15@gmail.com>
2025-09-02 20:57:30 +0100

    memo in practice

--- 12-atomic-blog/src/index.js
+++ 12-atomic-blog/src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
 // import App from "./App";
-import App from "./App-v2";
+// import App from "./App-v2";
+import App from "./App-memo";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
