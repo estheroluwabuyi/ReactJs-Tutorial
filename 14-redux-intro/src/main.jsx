e6e80cbe59a01939c41ef0243137c9b0207df992
Esther Okiki <estherokiki15@gmail.com>
2025-11-24 18:47:52 +0100

    Dispatching actions from our react app

--- 14-redux-intro/src/main.jsx
+++ 14-redux-intro/src/main.jsx
@@ -5,8 +5,8 @@ import "./index.css";
 import App from "./App.jsx";
 
 import store from "./store";
-store.dispatch({ type: "account/deposit", payload: 300 });
-console.log(store.getState());
+// store.dispatch({ type: "account/deposit", payload: 300 });
+// console.log(store.getState());
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
